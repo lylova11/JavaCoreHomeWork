8e79f5160862c9e070519a2bfebaecc5303ec2df
lylova11 <nemova94@mail.ru>
2022-03-30 19:54:32 +0300

    hw9

---
+++ hw9/LambdaExample.java
@@ -0,0 +1,40 @@
+package hw9;
+
+import java.util.Comparator;
+import java.util.Date;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class LambdaExample {
+
+    public static void main(String[] args) {
+
+        Function<String, Integer> function = (str)->{
+            return str.length();
+        };
+        System.out.println(function.apply("1111111111"));
+
+
+        Predicate<String> predicate = (str)->{
+            return str.length() > 100;
+        };
+        System.out.println(predicate.test("1111111111"));
+
+
+        Consumer<String> consumer = (str)->{
+            System.out.println(str.length());
+        };
+        consumer.accept("1111111111");
+
+
+        Supplier<Date> supplier = () -> new Date();
+        System.out.println(supplier.get());
+
+
+        Comparator<Integer> comparator = ((o1, o2) -> o1-o2);
+        System.out.println(comparator.compare(100,77));
+
+    }
+}
---
+++ hw9/StreamApiExample.java
@@ -0,0 +1,50 @@
+package hw9;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class StreamApiExample {
+
+    static ArrayList<String> stringArrayList = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+        createList();
+
+        Iterator<String> iterator = stringArrayList.iterator();
+        Set<Integer> filterList = new HashSet<>();
+        while (iterator.hasNext()){
+            String str = iterator.next();
+            if(str.length() < 3) filterList.add(Integer.valueOf(str));
+        }
+
+        System.out.println(filterList);
+
+        HashSet<Integer> filterList2 = new HashSet<>();
+        for (String str: stringArrayList){
+            if(str.length() < 3) filterList2.add(Integer.valueOf(str));
+        }
+
+        System.out.println(filterList2);
+
+
+        Set<Integer> filterList3 = stringArrayList.stream()
+                .filter((str)->{
+                    return str.length() < 3;
+                })
+                .map((str)-> Integer.valueOf(str))
+                .collect(Collectors.toSet());
+
+        System.out.println(filterList3);
+
+    }
+
+    public static void createList(){
+
+        Random random = new Random();
+        for(int i =0;i<100000;i++){
+            stringArrayList.add(String.valueOf(random.nextInt(1000000)));
+        }
+
+    }
+}
